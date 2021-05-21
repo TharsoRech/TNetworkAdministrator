15509a845bbc449020ed235f33618bf994a8071e
TharsoRech <tharso_rech@hotmail.com>
2021-05-21 17:25:58 -0300

    Move Objects iN Map

--- TNetworkAdministrator/Controls/MapControl.cs
+++ TNetworkAdministrator/Controls/MapControl.cs
@@ -12,9 +12,16 @@ namespace TNetworkAdministrator.Controls
     public partial class MapControl : UserControl
     {
         public bool online { get; set; }
+        private Point MouseDownLocation;
         public MapControl()
         {
             InitializeComponent();
+            this.MouseDown += (sender, e) => { MapControl_MouseDown(sender,e); };
+            this.MouseMove += (sender, e) => { MapControl_MouseMove(sender, e); };
+            DeviceImage.MouseDown += (sender, e) => { MapControl_MouseDown(sender, e); };
+            DeviceImage.MouseMove += (sender, e) => { MapControl_MouseMove(sender, e); };
+            Description.MouseDown += (sender, e) => { MapControl_MouseDown(sender, e); };
+            Description.MouseMove += (sender, e) => { MapControl_MouseMove(sender, e); };
         }
 
         private void StatusText_Click(object sender, EventArgs e)
@@ -46,5 +53,24 @@ namespace TNetworkAdministrator.Controls
                 throw;
             }
         }
+
+
+        private void MapControl_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                MouseDownLocation = e.Location;
+            }
+        }
+
+        private void MapControl_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                this.Left = e.X + this.Left - MouseDownLocation.X;
+                this.Top = e.Y + this.Top - MouseDownLocation.Y;
+            }
+        }
+
     }
 }
